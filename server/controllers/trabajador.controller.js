158e4a8750bdaf620ee2f3216465cb8ce93521d6
ZMrenoow <zmrenoow@gmail.com>
2024-11-25 18:05:15 -0300

    actualización del contralador del trabajador y del css de clientesAdm y TrabajadoresAdm

--- server/controllers/trabajador.controller.js
+++ server/controllers/trabajador.controller.js
@@ -11,15 +11,22 @@ export const getAllWorkerReservations = async (req, res) => {
                 r.id_reserva,
                 r.fecha_reserva,
                 r.hora_reserva,
-                e.tipo_estado AS estado
+                e.tipo_estado AS estado,
+                s.nombre_servicio,
+                u.nombre AS nombre_cliente
             FROM reserva_trabajador rt
             JOIN reservas r ON rt.id_reserva = r.id_reserva
             JOIN estados e ON r.estado = e.id_estado
+            JOIN carrito c ON r.id_carrito = c.id_carrito
+            JOIN cotizaciones co ON c.id_cotizacion = co.id_cotizacion
+            JOIN servicios s ON co.id_servicio = s.id_servicio
+            JOIN usuario u ON r.id_usuario = u.id_usuario
             WHERE rt.id_usuario_rol = (
                 SELECT ur.id_usuario_rol
                 FROM usuario_rol ur
                 WHERE ur.id_usuario = ?
-            )
+            );
+
             `,
             [req.user.id_usuario]
         );
